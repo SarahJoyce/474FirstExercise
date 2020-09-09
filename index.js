ce7534696d2c088cbda9a99d0776ab2c4eec13d0
Sarah Joyce <sjoyce@udel.edu>
2020-09-09 12:12:46 -0400

    finished jquery exercise

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 $(function(){
     //this code runs after page is fully loaded
+    $(document).on('submit', function(event){
+        event.preventDefault();
+        var fname = $('#fname').val();
+        var lname = $('#lname').val();
+        $("div.jumbotron h1").text(lname + ", " + fname);
+        $("div.jumbotron").removeClass('d-none');
+        return false;
+    })
 });
